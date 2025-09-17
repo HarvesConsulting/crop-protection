649a17cca0c266e5d7725a68ac5e358b896bbd58
Harves Consulting <lashyn.aleksandr@gmail.com>
2025-09-17 15:04:32 +0300

    Update App.js: змінив вигляд сторінки

--- src/diseases.js
+++ src/diseases.js
@@ -1,13 +1,17 @@
 // src/diseases.js
 
 export function isGrayMoldRisk(day) {
-  return day.wetHours >= 15 && day.wetTempAvg >= 17 && day.wetTempAvg <= 25;
+  return day.wetHours >= 6 && day.wetTempAvg >= 15 && day.wetTempAvg <= 28;
 }
 
 export function isAlternariaRisk(day) {
-  return day.wetHours >= 8 && day.allTempAvg >= 20 && day.allTempAvg <= 30;
+  return day.wetHours >= 5 && day.allTempAvg >= 15 && day.allTempAvg <= 30;
 }
 
 export function isBacterialRisk(day, rainValue) {
-  return rainValue >= 5 && day.allTempAvg >= 25 && day.allTempAvg <= 32;
+  return (
+    rainValue >= 3 &&
+    day.allTempAvg >= 22 && day.allTempAvg <= 32 &&
+    day.wetHours >= 4
+  );
 }
