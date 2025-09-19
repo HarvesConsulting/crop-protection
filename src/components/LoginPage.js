63d21ad34d856caded57a9df549809c8d507df11
Harves Consulting <lashyn.aleksandr@gmail.com>
2025-09-19 18:29:29 +0300

    Update App.js: змінив вигляд сторінки

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -127,11 +127,11 @@ const cardStyle = {
 
 const inputStyle = {
   width: "100%",
-  padding: "14px",
-  marginBottom: "14px",
+  padding: "14px 16px", // 14px зверху/знизу, 16px зліва/справа
   border: "1px solid #ccc",
   borderRadius: "6px",
   fontSize: "16px",
+  boxSizing: "border-box",
 };
 
 const buttonStyle = {
