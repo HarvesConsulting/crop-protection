ebec1c9a6bbd025bacd7aeb8a6210d7a5e1926d2
Harves Consulting <lashyn.aleksandr@gmail.com>
2025-10-05 12:09:16 +0300

    одано lucide-react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
-  // важливо: вимикаємо reset від Tailwind
-  corePlugins: { preflight: false },
-  theme: { extend: {} },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // ❗️ВАЖЛИВО: каже Tailwind'у, де шукати класи
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        tractorMove: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+      },
+      animation: {
+        tractorMove: 'tractorMove 4s linear infinite',
+      },
+    },
+  },
   plugins: [],
 };
