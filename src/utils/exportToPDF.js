e7a9d9c17a1fc45e06f06012b4dcf434ee3a827d
Harves Consulting <lashyn.aleksandr@gmail.com>
2025-10-14 16:25:54 +0300

    відновлено з боєм 💪

--- src/utils/exportToPDF.js
+++ src/utils/exportToPDF.js
@@ -1,10 +1,11 @@
 // src/utils/exportToPDF.js
 import jsPDF from "jspdf";
 import autoTable from "jspdf-autotable";
+import "../fonts/roboto-normal"; // 🔗 шлях до згенерованого шрифта
 
 export const exportToPDF = (data, logoUrl) => {
   const doc = new jsPDF();
-
+  doc.setFont("roboto");
   const img = new Image();
   img.src = logoUrl;
 
@@ -12,12 +13,12 @@ export const exportToPDF = (data, logoUrl) => {
     doc.addImage(img, "PNG", 10, 10, 40, 15);
 
     doc.setFontSize(16);
-    doc.setFont("helvetica", "bold");
+    doc.setFont("roboto", "bold");
     doc.text("Інтегрована система захисту рослин", 105, 30, { align: "center" });
 
     const today = new Date().toLocaleDateString("uk-UA");
     doc.setFontSize(10);
-    doc.setFont("helvetica", "normal");
+    doc.setFont("roboto", "normal");
     doc.text(`Дата формування: ${today}`, 200, 10, { align: "right" });
 
     const tableData = data.map((row) => [
@@ -31,7 +32,7 @@ export const exportToPDF = (data, logoUrl) => {
       head: [["Дата", "Препарат(и)", "Хвороби"]],
       body: tableData,
       styles: {
-        font: "helvetica",
+        font: "roboto",
         fontSize: 10,
         cellPadding: 3,
         valign: "top",
