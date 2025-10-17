840f8ac9bbd95a4efefa11072a2089a16f40dde4
Harves Consulting <lashyn.aleksandr@gmail.com>
2025-10-17 11:22:29 +0300

    відновлено з боєм 💪

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -15,12 +15,12 @@ export default function LogoutButton({ onLogout }) {
   return (
     <button
       onClick={handleLogout}
-      className="logout-btn-glass"
+      className="logout-btn-glass p-2"
       title="Вийти з акаунту"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="w-4 h-4 mr-2"
+        className="w-4 h-4"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
@@ -32,7 +32,6 @@ export default function LogoutButton({ onLogout }) {
           d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a2 2 0 01-2 2H6a2 2 0 01-2-2V7a2 2 0 012-2h5a2 2 0 012 2v1"
         />
       </svg>
-      Вийти
     </button>
   );
 }
