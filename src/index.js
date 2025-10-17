509651e5ef0021d4cce8601675d21b3954034ed6
Harves Consulting <lashyn.aleksandr@gmail.com>
2025-10-17 10:03:16 +0300

    відновлено з боєм 💪

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 ﻿import "./index.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import './i18n'; // важливо: підключаємо i18n перед рендером App
+import './i18n'; // або import './i18n/index.js'
 import App from "./App";
 
 // Створюємо корінь React
