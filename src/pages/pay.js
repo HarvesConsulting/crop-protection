95cb212a6b94a906e563b2588fa97c9dc1d17cfe
Harves Consulting <lashyn.aleksandr@gmail.com>
2025-10-22 21:58:36 +0300

    Fix all exports in googleSheetsAPI.js

--- src/pages/pay.js
+++ src/pages/pay.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "../styles/PayScreen.css"; // новий css для стилів
+import "../styles/SplashScreen.css";
 
 export default function PayPage() {
   const [email, setEmail] = useState("");
